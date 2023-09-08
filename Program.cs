62d32a61e0d00c475308ef51fd5ffdeb8870b8fc
Nilton Rocha Barboza <nilton.barboza@spassu.com.br>
2023-09-08 11:44:48 -0300

    Push code repo

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "f53cfbdf-e521-466a-8023-4e8435c01507";
+        private const string _tenantId = "2f0bfa6b-57c1-440c-8d11-30da125cd8a3";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build(); 
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
